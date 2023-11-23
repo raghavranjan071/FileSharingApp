13ab31abeba5a2d5b56a38098d8401232d3cb189
Raghav <raghav04601ranjan@gmail.com>
2023-11-23 14:25:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useRef, useState, useEffect } from 'react';
+import './App.css';
+import { uploadFile  } from './services/api';
+function App() {
+
+  const[file,setFile] = useState('');
+  const [result,setResult] = useState('');
+
+  const fileInputRef = useRef();
+  
+  const logo = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
+
+  useEffect(() =>{
+    const getImage = async() =>{
+      if(file){
+        const data = new FormData();
+        data.append('name',file.name);
+        data.append('file',file);
+
+        let response = await uploadFile(data);
+      }
+    }
+    getImage();
+  }, [file]);
+
+  const onUploadClick = () =>{
+    fileInputRef.current.click();
+  }
+
+  console.log(file);
+
+  return (
+    <div className='container'>
+    <img src={logo} className='banner' />
+    <div className='wrapper'>
+      <h1>Simple file sharing!</h1>
+      <p>Upload and share the download link.</p>
+      
+      <button onClick={() => onUploadClick()}>Upload</button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+
+      <a href={result} target='_blanks'>{result}</a>
+    </div>
+  </div>
+  );
+}
+
+export default App;
